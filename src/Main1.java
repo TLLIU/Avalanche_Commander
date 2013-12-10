7615394b25af0ade87adeea73487ee2704ed69ba
Tailei Liu <tailei.liu@spirent.com>
2013-12-10 15:33:53 +0800

    Revert "delete"
    This reverts commit 8df143e5e0f6369ae2f3f8514954ab31eea7569e.

---
+++ src/Main1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author avalanche
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Only a test");
+    }
+   public func();
+    
+   public func3();
+   public func2();
+}
+
+
